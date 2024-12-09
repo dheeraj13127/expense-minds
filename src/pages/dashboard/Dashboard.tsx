da807c32753fffab153aabd94bf61cd131c70558
dheeraj13127 <dheerajkumar13127@gmail.com>
2024-12-10 00:29:30 +1100

    added profile and error page

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -1,6 +1,11 @@
 import { Route, Routes } from "react-router-dom";
 import Records from "../records/Records";
 import Transactions from "../transactions/Transactions";
+import Error from "./components/error/Error";
+import Statistics from "../statistics/Statistics";
+import Tools from "../tools/Tools";
+import Settings from "../settings/Settings";
+import Profile from "../profile/Profile";
 
 const Dashboard = () => {
   return (
@@ -8,6 +13,11 @@ const Dashboard = () => {
       <Routes>
         <Route path="/records" element={<Records />} />
         <Route path="/transactions/*" element={<Transactions />} />
+        <Route path="/statistics" element={<Statistics />} />
+        <Route path="/tools" element={<Tools />} />
+        <Route path="/settings/*" element={<Settings />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="*" element={<Error />} />
       </Routes>
     </div>
   );
