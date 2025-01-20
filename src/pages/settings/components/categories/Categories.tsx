eba1bd13887413685dadf3d3395af7862b18e760
dheeraj13127 <dheerajkumar13127@gmail.com>
2025-01-20 23:15:58 +0530

    added CRUD functionalities to the categories.incoem

--- src/pages/settings/components/categories/Categories.tsx
+++ src/pages/settings/components/categories/Categories.tsx
@@ -99,6 +99,45 @@ const Categories = () => {
           )}
         </div>
       </div>
+      <div className=" col-span-12 mt-10">
+        <div className="bg-black w-full px-4 py-8 rounded">
+          <div className="flex items-center justify-between">
+            <p className="text-white text- font-poppins">Income</p>
+            <div>
+              <button
+                onClick={() => handleCreateCategoryModal("income")}
+                className=" text-black text-sm font-inter bg-white px-3 py-1 rounded hover:bg-opacity-90 duration-150"
+              >
+                Add
+              </button>
+            </div>
+          </div>
+          {userDetails?.categories.income.length !== 0 ? (
+            <div className="grid grid-cols-2 md:grid-cols-4 gap-x-4 gap-y-6 mt-10">
+              {userDetails?.categories.income.map(
+                (
+                  ce: UserSliceStateType["categories"]["income"][0],
+                  num: number
+                ) => (
+                  <div key={num} className="">
+                    <p
+                      onClick={() => handleUpdateCategoryModal(ce, "income")}
+                      className="text-white font-poppins cursor-pointer hover:bg-opacity-80 duration-150 text-sm text-center bg-gray-800 px-1 py-2 rounded"
+                    >
+                      {ce.categoryName}{" "}
+                      <span className="ml-1.5">{ce.categorySymbol}</span>
+                    </p>
+                  </div>
+                )
+              )}
+            </div>
+          ) : (
+            <div className="font-poppins text-white text-center my-20">
+              Loading...
+            </div>
+          )}
+        </div>
+      </div>
     </div>
   );
 };
